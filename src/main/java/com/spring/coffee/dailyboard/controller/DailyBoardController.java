5c5da7b160d113be92460fb43183a678d41d59ae
Min <37803032+MichaelKim0721@users.noreply.github.com>
2024-01-30 10:33:49 +0900

    dailyBoardController

--- src/main/java/com/spring/coffee/dailyboard/controller/DailyBoardController.java
+++ src/main/java/com/spring/coffee/dailyboard/controller/DailyBoardController.java
@@ -1,9 +1,18 @@
 package com.spring.coffee.dailyboard.controller;
 
+import java.io.File;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,12 +29,50 @@ public class DailyBoardController {
 	
 	@RequestMapping("dailyBoardList")
 	public @ResponseBody ModelAndView dailyBoardList(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		System.out.println("dailyBoardList 메소드");
 		ModelAndView mav = new ModelAndView();
 		
-		
-		mav.addObject("center", viewPath + "/dailyBoardList.jsp");
+		mav.addObject("isBoard", true);
+		mav.addObject("center", viewPath + "dailyBoardList.jsp");
 		mav.setViewName("main");
+		
+		return mav;
+	}
+	
+	@RequestMapping("dailyBoardForm")
+	public ModelAndView dailyBoardForm(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.addObject("isBoard", true);
+		mav.addObject("center", viewPath + "boardForm.jsp");
+		mav.setViewName("main");
+		
+		return mav;
+	}
+	
+	@RequestMapping("addDailyBoard")
+	public ModelAndView addDailyBoard(
+			/* @RequestParam("file") List<MultipartFile> files, */MultipartHttpServletRequest request) throws Exception {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		request.setCharacterEncoding("UTF-8");
+		String absPath = uploadPath + "/dailyBoard/";
+		
+		Map map = new HashMap();
+		
+		Enumeration enu = request.getParameterNames();
+		
+		while (enu.hasMoreElements()) {
+			String key = (String)enu.nextElement();
+			
+			String value = request.getParameter(key);
+			
+			map.put(key, value);
+		}
+		
+		
+		
+		
 		
 		return mav;
 	}
--- src/main/java/com/spring/coffee/mapper/DailyBoardMapper.java
+++ src/main/java/com/spring/coffee/mapper/DailyBoardMapper.java
@@ -1,7 +1,9 @@
 package com.spring.coffee.mapper;
 
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -10,5 +12,11 @@ import com.spring.coffee.dailyboard.vo.DailyBoardVO;
 @Mapper
 public interface DailyBoardMapper {
 	
+	@Select("SELECT * FROM dailyBoard")
+	List<DailyBoardVO> getDailyBoardList();
+	
+	@Insert("INSERT INTO dailyBoard(no, title, content, nickname, fileName) "
+			+ " VALUES (#{no}, #{title}, #{content}, #{nickname}, #{fileName})")
+	void addDailyBoard(Map map);
 	
 }
